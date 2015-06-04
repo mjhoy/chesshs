935235e479d91e3ea9ec2383d558c9c394863861
Michael Hoy <mjh@mjhoy.com>
2015-06-04 17:17:58 -0400

    use STArray for the board

--- Chess.hs
+++ Chess.hs
@@ -1,6 +1,8 @@
 module Chess where
 
-import Data.List (intercalate)
+import Control.Monad.ST
+import Control.Monad (foldM_)
+import Data.Array.ST
 
 data Player = Black | White
             deriving (Show)
@@ -16,6 +18,12 @@ data Piece = Pawn
 data Square = Square Player Piece
             | Empty
 
+data Rank = R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8
+          deriving (Show, Eq, Ord, Enum, Bounded, Ix)
+
+data File = FA | FB | FC | FD | FE | FF | FG | FH
+          deriving (Show, Eq, Ord, Enum, Bounded, Ix)
+
 instance Show Square where
   show (Square White King)   = "♔"
   show (Square White Queen)  = "♕"
@@ -30,29 +38,35 @@ instance Show Square where
   show (Square Black Knight) = "♞"
   show (Square Black Pawn)   = "♟"
   show Empty = "·"
-                         
-newtype Board = Board [[Square]]
 
-instance Show Board where
-  show (Board rows)   = intercalate "\n" $ map showRow rows
-    where
-      showRow squares = intercalate " " $ map show squares
+type BoardArray s = STArray s (Rank, File) Square
 
--- ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
--- ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
--- · · · · · · · ·
--- · · · · · · · ·
--- · · · · · · · ·
--- · · · · · · · ·
--- ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
--- ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
-newGameBoard :: Board
-newGameBoard = Board [
-    blackPieces, blackPawns,
-    empty, empty, empty, empty,
-    whitePawns, whitePieces
-    ]
+initialABoard :: ST s (BoardArray s)
+initialABoard = do
+    arr <- newArray ((R1, FA), (R8, FH)) Empty
+    foldM_ updateBoard (arr, R1, FA) $ concat initialBoardArray
+    return arr
   where
+    updateBoard (arr, rank, FH) square = do
+      writeArray arr (rank, FH) square
+      return (arr, succ rank, FA)
+    updateBoard (arr, rank, file) square = do
+      writeArray arr (rank, file) square
+      return (arr, rank, succ file)
+
+    -- ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+    -- ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+    -- · · · · · · · ·
+    -- · · · · · · · ·
+    -- · · · · · · · ·
+    -- · · · · · · · ·
+    -- ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+    -- ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
+    initialBoardArray = [
+      blackPieces, blackPawns,
+      empty, empty, empty, empty,
+      whitePawns, whitePieces
+      ]
     pawns = take 8 $ repeat Pawn
     empty = take 8 $ repeat Empty
     toBlack = Square Black
@@ -69,3 +83,8 @@ newGameBoard = Board [
     blackPawns  = map toBlack pawns
     whitePieces = map toWhite pieces
     whitePawns  = map toWhite pawns
+
+-- ghci:
+-- let arr = runSTArray initialABoard
+-- arr ! (R8, FE)
+-- > ♔
